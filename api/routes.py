17170d414d4fdfefd99c8e03ea3b0d0cd0f4c567
Phobic283 <leonoim@gamil.com>
2026-03-06 19:57:11 -0300

    endpoint ultimos alunos

--- api/routes.py
+++ api/routes.py
@@ -1,9 +1,12 @@
 import logging
-
+import pandas as pd
 from fastapi import APIRouter, Request
 
 from api.schemas import PredictionRequest, PredictionResponse
 from models.prediction import prever_grupo_aluno
+from models.ranking import ultimos_alunos
+from models.prediction import prever_todos_alunos
+
 
 logger = logging.getLogger(__name__)
 
@@ -26,3 +29,29 @@ def prever(dados: PredictionRequest, request: Request):
         state.cluster_metricas,
     )
     return resultado
+
+@router.get("/ranking/ultimos")
+def ultimos_alunos_endpoint(request: Request, top: int = 10):
+
+    logger.info("ultimos estudantes endpoint chamado", extra={"top": top})
+
+    scaler = request.app.state.scaler
+    kmeans = request.app.state.kmeans
+    modelo_rf = request.app.state.modelo_rf
+    cluster_metricas = request.app.state.cluster_metricas
+
+    df = pd.read_csv("dataset/PEDE_PASSOS_DATASET_FIAP.csv")
+
+    df_pred = prever_todos_alunos(
+        df,
+        scaler,
+        kmeans,
+        modelo_rf,
+        cluster_metricas
+    )
+
+    ranking = ultimos_alunos(df_pred, top)
+
+    logger.info("Últimos alunos retornados com sucesso", extra={"num_alunos": len(ranking)})
+
+    return ranking.to_dict(orient="records")
